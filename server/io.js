2f4ae25bdbd6f24cbec517f8c504513a50828c8e
Gonzalo Ayuso <gonzalo123@gmail.com>
2014-09-07 18:36:22 +0200

    io clean

--- server/io.js
+++ server/io.js
@@ -10,10 +10,6 @@ expressApp.get('/emit', function (req, res) {
     res.json('OK');
 });
 
-expressApp.listen(3001, 'localhost', function () {
-    console.log('Express started');
-}, '0.0.0.0');
+expressApp.listen(3001);
 
-server.listen(3000, function () {
-    console.log('IO started');
-}, '0.0.0.0');
+server.listen(3000);
